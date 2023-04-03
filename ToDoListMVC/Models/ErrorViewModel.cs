529a4025ef14adf485aff4ead2fcf4eeb56fc88e
Dmytro Burylko <sana22_bdv@student.ztu.edu.ua>
2023-04-03 20:07:10 +0300

    Add project files.

---
+++ ToDoListMVC/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ToDoListMVC.Models
+{
+	public class ErrorViewModel
+	{
+		public string? RequestId { get; set; }
+
+		public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+	}
+}
